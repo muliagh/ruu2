9ea1be31328ce32fae79fdce2447772ffa9e5364
muliagh <102743815+muliagh@users.noreply.github.com>
2022-06-16 07:28:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,29 +11,29 @@ global.APIKeys = {
 }
 
 //===> ATUR BIAR KALIAN SENENG
-global.autoReadGc = true //Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
+global.autoReadGc = false //Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
 global.autoReadAll = false //Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
 global.anticall = true //Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
-global.available = false //Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
+global.available = true //Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
 global.autoTyping = false //Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
-global.autoRecord = true //Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
-global.multiplier = 69 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
-global.limitCount = 70 //70 Dah Pas Banh
+global.autoRecord = false //Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
+global.multiplier = 50 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
+global.limitCount = 100 //70 Dah Pas Banh
 global.prefa = ['','.','/','#']
 
 //===> UBAH DISINI
-global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
-global.namabot = 'Hiruko Kagetane' //Ganti Jadi Nama Botmu
-global.packname = 'Hiruko Kagetane' //Sticker Weem
-global.author = 'By Ryuuka Team' //Sticker Weem
+global.namaowner = 'MuliaBotz' //Ganti Jadi Namamu
+global.owner = ['6285161806141','6282322350815'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6285161806141' //Owner Utama Istilahnya
+global.namabot = 'MuliaBotz MD' //Ganti Jadi Nama Botmu
+global.packname = 'Random Sticker' //Sticker Weem
+global.author = 'By MuliaBotz MD' //Sticker Weem
 global.webme = 'https://api-ryubotz.herokuapp.com' //Ganti Sama Web Mu
 global.linkgroup = 'https://chat.whatsapp.com/BhOYdoWNRFC4n4bdsEexlV' //Ganti Sama Link Gc Mu
 global.sessionName = 'session'
 global.youtube = 'https://youtube.com/c/RyuukaBotz' //Ganti Sama Link Yt Mu
 global.github = 'https://github.com/R-TEAM94' //Ganti Sama Link Github Mu
-global.instagram = 'https://instagram.com/fandyyy._' //Ganti Sama Link Ig mu
+global.instagram = 'https://instagram.com/sulthon.mulia' //Ganti Sama Link Ig mu
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
